6c8f1333a6419523f832e6b74536f2b0552c3c89
RafSwiggers <rafswiggers@gmail.com>
2019-08-02 13:52:11 +0200

    add email validation

--- form.php
+++ form.php
@@ -1,3 +1,12 @@
 <?php
 print_r($_POST);
+
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$name = filter_var($_POST['name'], FILTER_SANITIZE_EMAIL);
+if (true ==== filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "This cleaned email address is considered valid."
+} else {
+	echo "This cleaned email address is not valid. Sorry. xoxo."
+}
+
 ?>
