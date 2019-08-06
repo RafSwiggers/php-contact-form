5493d882f879199f73b0abf3da66061f3cded426
RafSwiggers <rafswiggers@gmail.com>
2019-08-06 11:14:40 +0200

    add the mailer function

--- form.php
+++ form.php
@@ -1,79 +1,91 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
 $contact = $_POST["contact"];
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$name = filter_var($_POST['name'], FILTER_SANITIZE_EMAIL);
-$text = filter_var($_POST['text'], FILTER_SANITIZE_EMAIL);
-function Mailer() {
- 
-    require '../vendor/autoload.php';
-    //Create a new PHPMailer instance
-    $mail = new PHPMailer;
-    //Tell PHPMailer to use SMTP
-    $mail->isSMTP();
-    //Enable SMTP debugging
-    // 0 = off (for production use)
-    // 1 = client messages
-    // 2 = client and server messages
-    $mail->SMTPDebug = 2;
-    //Set the hostname of the mail server
-    $mail->Host = 'smtp.gmail.com';
-    // use
-    // $mail->Host = gethostbyname('smtp.gmail.com');
-    // if your network does not support SMTP over IPv6
-    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-    $mail->Port = 587;
-    //Set the encryption system to use - ssl (deprecated) or tls
-    $mail->SMTPSecure = 'tls';
-    //Whether to use SMTP authentication
-    $mail->SMTPAuth = true;
-    //Username to use for SMTP authentication - use full email address for gmail
-    $mail->Username = "wynkoerier@gmail.com";
-    //Password to use for SMTP authentication
-    $mail->Password = "Captain1988!";
-    //Set who the message is to be sent from
-    $mail->setFrom('wynkoerier@gmail.com', 'First Last');
-    //Set who the message is to be sent to
-    $mail->addAddress('wynkoerier@gmail.com', 'John Doe');
-    //Set the subject line
-    $mail->Subject = 'PHPMailer GMail SMTP test';
-    //Read an HTML message body from an external file, convert referenced images to embedded,
-    //convert HTML into a basic plain-text alternative body
-    $mail->msgHTML(file_get_contents($_POST), __DIR__);
-    //Replace the plain text body with one created manually
-    $mail->AltBody = 'This is a plain-text message body';
-    //send the message, check for errors
-    if (!$mail->send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-    } else {
-        echo "Message sent!";
-        //Section 2: IMAP
-        //Uncomment these to save your message in the 'Sent Mail' folder.
-        #if (save_mail($mail)) {
-        #    echo "Message saved!";
-        #}
-    }
-    //Section 2: IMAP
-    //IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-    //Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-    //You can use imap_getmailboxes($imapStream, '/imap/ssl', '*' ) to get a list of available folders or labels, this can
-    //be useful if you are trying to get this working on a non-Gmail IMAP server.
-    function save_mail($mail)
-    {
-        //You can change 'Sent Mail' to any other folder or tag
-        $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
-        //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-        $imapStream = imap_open($path, $mail->Username, $mail->Password);
-        $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-        imap_close($imapStream);
-        return $result;
-    }
-
-}
-
-
-
-?>
+$name = $_POST["name"];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+if ($contact) { 
+$clientEmail = $_POST["email"];
+$clientMessage = $_POST["text"];
+// define the form of the email the user will recieve
+$email_from = $clientEmail;
+$email_subject = "new email from $clientEmail";
+$email_body = "Hello owner of this webiste, you have recieved a message from $name. Below you can find the message:                   $clientMessage";
+// check if form contains data from the user
+    if (empty($name)){
+        echo '<head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                  </head>
+                  <body>
+                  <div class="alert alert-danger text-center"><p>Please fill in your name.</p></div>
+                  </body>';
+        exit;
+      } else if(empty($clientEmail)){
+        echo '<head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                  </head>
+                  <body>
+                  <div class="alert alert-danger text-center">Please fill in your email.<p></p></div>
+                  </body>';
+        exit;
+      } else if(empty($clientMessage)){
+        echo "*fill in your message*";
+        exit;
+      } else{
+        $mail;
+      }
+      // sanitize the email so that the value is correct
+      $sanitized_email = filter_var($clientEmail, FILTER_SANITIZE_EMAIL);
+      if (filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+          $mail;
+      } else{
+        echo '<head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                  </head>
+                  <body>
+                  <div class="alert alert-danger text-center"><p> This is not a valid email address</p></div>
+                  </body>';
+        exit;
+      }
+      // end email sanitizer. 
+      // end formchecker from the user. 
+      // Load Composer's autoloader
+      require 'vendor/autoload.php';
+      // Instantiation and passing `true` enables exceptions
+      $mail = new PHPMailer(true);
+      try {
+          //Server settings
+          $mail->isSMTP();                                            // Set mailer to use SMTP
+          $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+          $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+          $mail->Username   = 'wynkoerier@gmail.com';                     // SMTP username
+          $mail->Password   = 'Helios12!';                               // SMTP password
+          $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+          $mail->Port       = 587;                                    // TCP port to connect to
+          //Recipients
+          $mail->setFrom($clientEmail, $name);
+          $mail->addAddress('example@gmail.com', 'Jonas Dreessen');     // Add a recipient
+          // Content
+          $mail->isHTML(true);                                  // Set email format to HTML
+          $mail->Subject = $email_subject;
+          $mail->Body    = $email_body;
+          $mail->AltBody = $email_body;
+          $mail->send();
+          echo '<head>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          </head>
+          <body>
+          <div class="alert alert-danger text-center"><p> Thank you so much for filling in our contact form. We will get back to you as soon as possible!</p></div>
+          </body>';
+      } catch (Exception $e) {
+        
+          echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          </head>
+          <body>
+          <div class="alert alert-danger text-center"><p> Message could not be sent. Mailer Error:'{$mail->ErrorInfo}.'</div>
+          </body>';
+      }  
+      };
+      ?>
 
 <html>
     <head>
@@ -81,24 +93,4 @@ function Mailer() {
     </head>
     <body>
 
-
-<?php
-
-if($text){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-    if ($contact) {
-        echo "<div class='alert alert-danger text-center'><p> Thank you so much for filling in our contact form. We will get back to you as soon as possible!</p></div>";
-        Mailer();
-    } else {
-        echo "<div class='alert alert-danger text-center'><p>Thank you for your time and effort. We appreciate your feedback and will take it to heart! </p></div>";
-    }
-    } else {
-        echo "<div class='alert alert-danger text-center'><p>I'm sorry, It seems your email address was not valid.</p></div>";
-    }
-    
-    } else {
-        echo "<div class='alert alert-danger text-center'><p>Please enter a question or a remark in the text field.</p></div>";
-    }
-    
-    ?>
 
